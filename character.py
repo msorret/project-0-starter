e79b5958d609054b87e728347b7d49f173077c49
Ms. Orret <orret.deborah@pusd.us>
2024-08-28 20:35:18 +0000

    Example Game

--- character.py
+++ character.py
@@ -1,1 +1,18 @@
-
+class Character:
+    def __init__(self, name, description, inventory, dialogue, looking_for):
+        self.name = name
+        self.description = description
+        self.inventory = inventory
+        self.dialogue = dialogue
+        self.looking_for = looking_for
+    
+    def talk_to_player(self, player_msg):
+        for sentence in self.dialogue:
+            if player_msg == sentence:
+                print(self.name, "said", self.dialogue[sentence])
+    
+    def ask_for_item(self):
+        print("I am looking for a " + self.looking_for)
+    
+    def receive_item(self, item):
+        self.inventory.append(item)
